345fea446a685b8faae5c326993046b5dd93e0d0
bradgayheart <b.gayheart3@gmail.com>
2020-11-08 15:17:40 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+storeItems.forEach(function(item){
+    if(item.inStock === true) {
+      $('#appendToMe').append(`<div class="container contentContainer2">
+      <p class="price">$${item.price}</p><p class="itemName">${item.name}</p><p class="details">${item.details}</p>
+      </div>`);
+
+    }
+    
+    //else {
+    //  $('#appendToMe').append(`<p class="notInStock">${item.name}: $${item.price} Not in stock</p>`);
+    //}
+  });
+
+$('#clickMe').on("click",function() {
+    $('#contentContainer').toggleClass('darkMode');
+    $('.contentContainer2').each(function(){
+        $(this).toggleClass('darkModeContainer');    
+    })
+  })
